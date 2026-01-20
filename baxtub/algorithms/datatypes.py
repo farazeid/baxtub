d70aac9b8bd19854d84b236381711eca7ec1409a
farazeid <93294998+farazeid@users.noreply.github.com>
2026-01-20 09:44:43 +0000

    style: add + resolve type hints

--- baxtub/algorithms/datatypes.py
+++ baxtub/algorithms/datatypes.py
@@ -1,16 +1,16 @@
 from typing import Any
 
-import jax.numpy as jnp
+import jax
 from flax import struct
 
 
 @struct.dataclass
 class Transition:
-    obs: jnp.ndarray
-    action: jnp.ndarray
-    next_obs: jnp.ndarray
-    reward: jnp.ndarray
-    done: jnp.ndarray
-    info: jnp.ndarray
+    obs: jax.Array
+    action: jax.Array
+    next_obs: jax.Array
+    reward: jax.Array
+    done: jax.Array
+    info: jax.Array
     #
     extra: dict[str, Any]
